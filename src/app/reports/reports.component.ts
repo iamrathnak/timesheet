7394ddf61d99a72b3ebc33064ddb419ba0a01536
Rathna kumar <rathnamsec@gmail.com>
2020-02-16 12:19:09 +0000

    added report  model

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, QueryList, ViewChildren} from '@angular/core';
+import {Observable} from 'rxjs';
 
+import {Country} from './country';
+import {CountryService} from './country.service';
+import {NgbdSortableHeader, SortEvent} from './sortable.directive';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+
+  countries$: Observable<Country[]>;
+  total$: Observable<number>;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
+
+  constructor(public service: CountryService) {
+    this.countries$ = service.countries$;
+    this.total$ = service.total$;
+  }
+
+  onSort({column, direction}: SortEvent) {
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    this.service.sortColumn = column;
+    this.service.sortDirection = direction;
   }
 
 }
